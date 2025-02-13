dc486c2e7b52050fbecbbdecc2146548ca634b61
AdamJoestar <adamschoolauto@gmail.com>
2025-02-14 00:02:25 +0700

    css nya flutter bjir

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home : Text('Hey ho'),
-  ));
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'app gua ini',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'IndieFlower'
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'hello bang',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        backgroundColor: Colors.red[600],
+        child: Text(
+          'click',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+    ),
+  ),
+  );
 }
